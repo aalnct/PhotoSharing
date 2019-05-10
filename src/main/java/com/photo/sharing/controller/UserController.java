7131f8011ae665bbf50f92749d0ec2267027154a
Amit Agarwal <amit.agarwal@searshc.com>
2019-05-09 19:34:12 -0500

    JWT Token and Login

--- src/main/java/com/photo/sharing/controller/UserController.java
+++ src/main/java/com/photo/sharing/controller/UserController.java
@@ -2,11 +2,17 @@ package com.photo.sharing.controller;
 
 import com.photo.sharing.model.User;
 import com.photo.sharing.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by AmitAgarwal on 5/6/19.
@@ -18,9 +24,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String login(@RequestBody Map<String,String> json) throws ServletException{
+        if(json.get("username") == null || json.get("password") == null){
+            throw new ServletException("Please Fill in username/password");
+        }
+        String userName = json.get("username");
+        String password = json.get("password");
 
-    public String login(){
-        return null;
+        User user = userService.findByUserName(userName);
+        if(user == null){
+            throw new ServletException("UserName not Found");
+        }
+
+        String pwd = user.getPassword();
+        if(!password.equals(pwd)){
+            throw new ServletException("Invalid Login, usename & password does not match");
+        }
+
+
+        return Jwts.builder().setSubject(userName).claim("roles","user").setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
